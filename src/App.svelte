8eb31ce6900320fccbeb0085f628d95dc618ffca
untemps <v.lebadezet@untemps.net>
2021-12-08 18:08:29 +0100

    feat: Initial commit

---
+++ src/App.svelte
@@ -0,0 +1,22 @@
+<script>
+    import {Palette} from '@untemps/svelte-palette'
+</script>
+
+<main>
+    <Palette colors={['#ff0', '#f00', '#00f']}/>
+</main>
+
+<style>
+	main {
+		text-align: center;
+		padding: 1em;
+		max-width: 240px;
+		margin: 0 auto;
+	}
+
+	@media (min-width: 640px) {
+		main {
+			max-width: none;
+		}
+	}
+</style>
