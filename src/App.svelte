b1246b1f8463e816db805eeecd67b0083d9d89be
untemps <v.lebadezet@untemps.net>
2021-12-09 07:36:22 +0100

    feat: Style Palette component

--- src/App.svelte
+++ src/App.svelte
@@ -1,22 +1,51 @@
 <script>
-    import {Palette} from '@untemps/svelte-palette'
+	import { Palette } from '@untemps/svelte-palette'
+	const colors = [
+		'#865C54',
+		'#8F5447',
+		'#A65846',
+		'#A9715E',
+		'#AD8C72',
+		'#C2B091',
+		'#172B41',
+		'#32465C',
+		'#617899',
+		'#9BA2BC',
+		'#847999',
+		'#50526A',
+		'#8B8C6B',
+		'#97A847',
+		'#5B652C',
+		'#6A6A40',
+		'#F2D9BF',
+		'#F5BAAE',
+		'#F1A191',
+	]
+	let bgColor = colors[0]
+	let selectedColor = null
+
+    const _onColorSelect = ({ detail: { color } }) => bgColor = color
 </script>
 
-<main>
-    <Palette colors={['#ff0', '#f00', '#00f']}/>
+<main style="--bgColor:{bgColor}">
+    <Palette {colors} selectedColor={bgColor} allowDuplicates allowDeletion showTransparentSlot maxColors={30} class='palette' on:select={_onColorSelect} />
 </main>
 
 <style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
+    main {
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        height: 100%;
+        background-color: var(--bgColor);
+    }
 
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
+    :global(.palette) {
+        display: flex;
+        flex-direction: column;
+        row-gap: 1rem;
+        padding: 2rem;
+        background: white;
+        box-shadow: 0 0 10px 5px rgba(0,0,0,0.18);;
+    }
 </style>
